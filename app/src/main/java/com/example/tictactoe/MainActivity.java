71a95df7f3733f3980f46685f9da8d0f05d5073a
LessMathMoreMeth <killingdario@gmail.com>
2022-10-20 14:32:15 +0200

    Inizio creazione layout dell'activity di gioco

--- app/src/main/java/com/example/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/tictactoe/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean singleUser = false;
+    Button btnLocal;
+    Button btnMulti;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLocal = findViewById(R.id.btnLocal);
+        btnMulti = findViewById(R.id.btnMulti);
+
+        btnLocal.setOnClickListener(v -> {
+            singleUser = true;
+            startActivity(new Intent(getApplicationContext(), GameActivity.class));
+        });
+
+        btnMulti.setOnClickListener(v -> {
+            singleUser = false;
+            startActivity(new Intent(getApplicationContext(), GameActivity.class));
+        });
     }
 }
